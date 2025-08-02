d218f85d79338bb077d5fab84f081c7244482004
Darshan Prajapati <darshupr2710@gmail.com>
2025-08-02 06:08:16 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='your_render_postgresql_url_here',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
